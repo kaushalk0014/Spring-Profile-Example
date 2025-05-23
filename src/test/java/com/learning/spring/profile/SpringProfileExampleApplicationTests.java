5de43dc7e05947cf373110ca600b9173a437ea07
kaushal kumar <kaushal.k.0014@gmail.com>
2025-05-23 20:53:41 +0530

    initial commit

---
+++ src/test/java/com/learning/spring/profile/SpringProfileExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfileExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
